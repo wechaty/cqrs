4caa9d30534c6594eb996757d3275801e5a6ce53
Huan LI (李卓桓) <zixia@zixia.net>
2022-03-15 12:47:45 +0800

    add execute unit tests

---
+++ src/execute$/execute$.spec.ts
@@ -0,0 +1,135 @@
+#!/usr/bin/env -S node --no-warnings --loader ts-node/esm
+
+/**
+ *   Wechaty Open Source Software - https://github.com/wechaty
+ *
+ *   @copyright 2016 Huan LI (李卓桓) <https://github.com/huan>, and
+ *                   Wechaty Contributors <https://github.com/wechaty>.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+import {
+  test,
+  testSchedulerRunner,
+}                         from 'tstest'
+import { GError }         from 'gerror'
+import {
+  of,
+  Subject,
+}                         from 'rxjs'
+import {
+  map,
+  filter,
+  mergeMap,
+}                         from 'rxjs/operators'
+import { isActionOf }     from 'typesafe-actions'
+
+import * as CqrsDuck  from '../duck/mod.js'
+
+import { TIMEOUT_MS } from './constants.js'
+import { execute$ }   from './execute$.js'
+
+test('execute$() message creator / in time', testSchedulerRunner(m => {
+  const PUPPET_ID   = 'puppet-id'
+  const CONTACT_ID  = 'contact-id'
+
+  const query = CqrsDuck.actions.getCurrentUserIdQuery(PUPPET_ID)
+  const message = CqrsDuck.actions.currentUserIdGotMessage({
+    ...query.meta,
+    contactId : CONTACT_ID,
+  })
+
+  const values = {
+    m: message,
+    q: query,
+  }
+
+  const source    = '-q--'
+  const bus       = '---m'
+  const expected  = '---m'
+
+  const mockBus$ = m.hot(bus, values)
+  const result$ = m.hot(source, { q: values.q }).pipe(
+    filter(isActionOf(CqrsDuck.actions.getCurrentUserIdQuery)),
+    mergeMap(execute$(mockBus$)(CqrsDuck.actions.currentUserIdGotMessage)),
+  )
+
+  m.expectObservable(result$).toBe(expected, values)
+}))
+
+test('execute$() with message creator / timeout', testSchedulerRunner(m => {
+  const PUPPET_ID = 'puppet-id'
+  const GERROR    = 'Timeout has occurred'
+
+  const query = CqrsDuck.actions.getCurrentUserIdQuery(PUPPET_ID)
+  const message = CqrsDuck.actions.currentUserIdGotMessage({
+    gerror    : GERROR,
+    id        : query.meta.id,
+    puppetId  : query.meta.puppetId,
+  })
+
+  const values = {
+    m: message,
+    q: query,
+  }
+
+  const source    = `q ${TIMEOUT_MS - 1}ms -`
+  const expected  = `- ${TIMEOUT_MS - 1}ms m`
+
+  const dummyBus$ = new Subject<any>()
+
+  const normalizeMessage = (message: ReturnType<typeof CqrsDuck.actions.currentUserIdGotMessage>) => ({
+    ...message,
+    meta: {
+      ...message.meta,
+      gerror: GError.from(message.meta.gerror).message,
+    },
+  })
+
+  const result$ = m.hot(source, { q: values.q }).pipe(
+    filter(isActionOf(CqrsDuck.actions.getCurrentUserIdQuery)),
+    mergeMap(execute$(dummyBus$)(CqrsDuck.actions.currentUserIdGotMessage)),
+    map(normalizeMessage),
+  )
+
+  m.expectObservable(result$).toBe(expected, values)
+}))
+
+test('execute$() without message creator', testSchedulerRunner(m => {
+  const PUPPET_ID   = 'puppet-id'
+  const CONTACT_ID  = 'contact-id'
+
+  const query   = CqrsDuck.actions.getCurrentUserIdQuery(PUPPET_ID)
+  const message = CqrsDuck.actions.currentUserIdGotMessage({
+    ...query.meta,
+    contactId: CONTACT_ID,
+  })
+
+  const values = {
+    m: message,
+    q: query,
+  }
+
+  const source    = '-q-- 9m |'
+  const bus       = '---m     '
+  const expected  = '---- 9m |'
+
+  const mockBus$ = m.hot(bus, values)
+  const result$ = m.hot(source, { q: values.q }).pipe(
+    filter(isActionOf(CqrsDuck.actions.getCurrentUserIdQuery)),
+    mergeMap(execute$(mockBus$)()),
+  )
+
+  m.expectObservable(result$).toBe(expected, values)
+}))
