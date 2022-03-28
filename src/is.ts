f302422b4572de6f0f42881a78dc262ab48b5c95
Huan (李卓桓) <zixia@zixia.net>
2022-03-28 23:15:09 +0800

    declare is

--- src/is.ts
+++ src/is.ts
@@ -1,5 +1,7 @@
-import { isActionOf as is }   from 'typesafe-actions'
+import { isActionOf }   from 'typesafe-actions'
 
-export {
-  is,
-}
+/**
+ * check if an action is the instance of given action-creator(s)
+ *  (curried assert function)
+ */
+export const is: (...args: Parameters<typeof isActionOf>) => ReturnType<typeof isActionOf> = (...args) => isActionOf(...args)
